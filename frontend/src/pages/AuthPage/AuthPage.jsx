1b929accb0bc2b10b3d5b37c9e6bad761be1a7d6
Flazer <146836746+Flazer0136@users.noreply.github.com>
2025-09-28 15:49:31 -0600

    Added Address Fields
    - Added address fields in register page
    -  Added a Home page to avoid overlap of components in app.jsx

--- frontend/src/pages/AuthPage/AuthPage.jsx
+++ frontend/src/pages/AuthPage/AuthPage.jsx
@@ -3,8 +3,7 @@ import Register from './component/Register'
 const AuthPage = () => {
   return (
     <main className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
-      <section className="w-full max-w-lg">
-        <h1 className="text-3xl font-bold mb-6 text-center">Create an Account</h1>
+      <section className="w-full max-w-xl">
         <Register />
       </section>
     </main>

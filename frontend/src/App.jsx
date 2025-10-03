5e3a81e25eef0aeec3b9cd476b5d05eed82897be
Johnsinoy <jcsinoy91@gmail.com>
2025-10-03 15:54:23 -0600

    Messaging and negotiation between clients and providers

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,30 @@
-import React from 'react'
-import { Routes, Route } from "react-router-dom"
-import Home from './pages/home'
-import AuthPage from './pages/AuthPage/AuthPage'
+import { Routes, Route } from "react-router-dom";
+
+// Pages
+import Home from "./pages/home";
+import AuthPage from "./pages/AuthPage/AuthPage";
+import ClientChatRoom from "./pages/Chat/ClientChatRoom.jsx";
+import ProviderChatRoom from "./pages/Chat/ProviderChatRoom.jsx";
+import BookTask from "./pages/Chat/BookTask.jsx";
 
 function App() {
   return (
-  <div>
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/auth" element={<AuthPage />} />
-    </Routes>
-  </div>
+    <div className="min-h-screen bg-gray-50">
+      <Routes>
+        {/* Public Routes */}
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthPage />} />
+
+        {/* Chat Routes */}
+        <Route path="/chat/client/:bookingId" element={<ClientChatRoom />} />
+        <Route path="/chat/provider/:bookingId" element={<ProviderChatRoom />} />
+
+        {/* Booking Routes */}
+        <Route path="/book" element={<BookTask />} />
+        <Route path="/book/:taskId" element={<BookTask />} />
+      </Routes>
+    </div>
   );
 }
 
-export default App
+export default App;
